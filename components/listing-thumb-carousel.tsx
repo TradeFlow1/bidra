56de29f3169cbc5bc131acad1c0a1606ef48e346
Jean-Pierre Du Plessis <jpdup491@gmail.com>
2026-01-04 10:13:06 +1000

    Polish carousel no-image state with centered icon

--- components/listing-thumb-carousel.tsx
+++ components/listing-thumb-carousel.tsx
@@ -139,12 +139,17 @@ function scrollTo(i: number) {
   }, [imgs.length]);
 
   if (!imgs.length) {
-    return (
-      <div className="flex h-full w-full items-center justify-center text-xs text-neutral-500">
-        No image
-      </div>
-    );
-  }
+  return (
+    <div className="flex h-full w-full items-center justify-center bg-neutral-100">
+      <img
+        src="/brand/icon/bidra-icon_dark.png"
+        alt="No image"
+        className="h-16 w-16 object-contain opacity-70"
+        draggable={false}
+      />
+    </div>
+  );
+}
 
   return (
     <div
