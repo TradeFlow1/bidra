eb9412404b6e37a1832b61c37a69b8154f57194c
Jean-Pierre Du Plessis <jpdup491@gmail.com>
2026-01-19 18:12:02 +1000

    fix: repair metadata + set favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import Providers from "@/components/providers";
 export const metadata = {
   title: "Bidra",
   description: "An Australian marketplace to buy and sell items safely",
+  icons: {
+    icon: "/brand/bidra-kangaroo-icon.png",
+  },
 };
 
 export default function RootLayout({
